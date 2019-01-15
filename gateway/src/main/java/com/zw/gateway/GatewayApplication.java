79650a0ffe2f351a206e0ee950408126d3d47bf0
zhengwei <wei.zheng@peanut.ai>
2019-01-15 18:02:03 +0800

    fix

--- gateway/src/main/java/com/zw/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/zw/gateway/GatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication(scanBasePackages = {"com.zw.gateway"})
+@SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 public class GatewayApplication {
