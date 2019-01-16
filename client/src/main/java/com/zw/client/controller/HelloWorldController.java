cf7af742fd28acca30eaae4de5f75fc188aafbcb
zhengwei <wei.zheng@peanut.ai>
2019-01-16 16:45:09 +0800

    集成Zipkin

--- client/src/main/java/com/zw/client/controller/HelloWorldController.java
+++ client/src/main/java/com/zw/client/controller/HelloWorldController.java
@@ -1,12 +1,14 @@
 package com.zw.client.controller;
 
 import com.zw.server.api.service.HelloWorldService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@Slf4j
 public class HelloWorldController {
 
     @Resource
